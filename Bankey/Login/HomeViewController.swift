c8d633c565f137b15bca1193bcfed15f0608338a
Juan Arbelaez <juan.arbelaez316@gmail.com>
2025-01-19 17:25:54 -0500

    Adicion archivo .swift HomeViewController

---
+++ Bankey/Login/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  Bankey
+//
+//  Created by Juan Arbelaez on 19/01/25.
+//
+
+import Foundation
+
+
+class HomeViewController{
+    
+    var View: HomeViewController = HomeViewController()
+    
+    
+    func config(){
+        
+        
+    }
+    
+}
